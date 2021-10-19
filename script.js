3016a58864799aa467eef88e91c545ef9cadfdff
Ben <benbperry1@gmail.com>
2021-10-18 20:43:36 -0700

    1st update-added background

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+function snowButton() {
+    document.getElementById("body").style.backgroundImage = "url(./images/shuksan.jpg)";
+ }
+
+ function desertButton() {
+    document.getElementById("body").style.backgroundImage = "url(./images/desert3.jpg)";
+ }
