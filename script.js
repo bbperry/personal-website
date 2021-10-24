ac07e512b3a27b686f85b4cfe0635b93e904fb5d
Ben <benbperry1@gmail.com>
2021-10-24 14:27:02 -0700

    added a lightbox feature for Travel Photos page

--- script.js
+++ script.js
@@ -11,7 +11,25 @@ function snowButton() {
  }
 
 
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
 
- function lightBox(){
 
- }
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+   image.addEventListener('click', e => {
+      lightbox.classList.add('active')
+      const img = document.createElement('img')
+      img.src = image.src
+      while (lightbox.firstChild) {
+         lightbox.removeChild(lightbox.firstChild)
+      }
+      lightbox.appendChild(img)
+   })
+})
+
+lightbox.addEventListener('click', e => {
+   if (e.target !== e.currentTarget) return
+   lightbox.classList.remove('active')
+})
